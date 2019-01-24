4d255b0d9aca12ada1d11c96acbf65b1ed2250f5
Romel levy <Romelestefan@gmail.com>
2019-01-23 20:32:28 -0500

    Initial Commit

---
+++ SoundPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundPlayer
+//
+//  Created by Romel Levy on 1/23/19.
+//  Copyright © 2019 Romel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
